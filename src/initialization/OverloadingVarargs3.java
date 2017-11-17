f809d6af3ea07ecfd50120c8f803b5b473f231af
zhzhip <zhzhip@DESKTOP-CF5S72A>
2017-11-17 10:26:38 +0800

    5.8 数组初始化

--- src/initialization/OverloadingVarargs3.java
+++ src/initialization/OverloadingVarargs3.java
@@ -2,17 +2,54 @@ package initialization;
 //: initialization/OverloadingVarargs3.java
 
 public class OverloadingVarargs3 {
-  static void f(float i, Character... args) {
-    System.out.println("first");
-  }
-  static void f(char c, Character... args) {
-    System.out.println("second");
-  }
-  public static void main(String[] args) {
-    f(1, 'a');
-    f('a', 'b');
-  }
-} /* Output:
-first
-second
-*///:~
+
+    static void f(float i, Character... args) {
+        System.out.println("first");
+    }
+
+    static void f(char c, Character... args) {
+        System.out.println("second");
+    }
+
+    public static void main(String[] args) {
+        f(1, 'a');
+        f('a', 'b');
+    }
+}
+
+// initialization/VarargEx19.java
+// TIJ4 Chapter Initialization, Exercise 19, page 204
+/*
+ * Write a method that takes a vararg String array. Verify that you can pass either a comma-separated list of Strings or
+ * a String[] into this method.
+ */
+
+class VarargEx19 {
+
+    static void showStrings(String... args) {
+        for (String s : args)
+            System.out.print(s + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        showStrings("one", "TWO", "three", "four");
+        showStrings(new String[] {"1", "2", "3", "4" });
+    }
+}
+
+// initialization/VarargEx20.java
+// TIJ4 Chapter Initialization, Exercise 20, page 204
+/*
+ * Create a main() that uses varargs instead of the ordinary main() syntax. Print all the elements in the resulting args
+ * array. Test it with various numbers of command-line arguments.
+ */
+
+class VarargEx20 {
+
+    public static void main(String... args) {
+        for (String s : args)
+            System.out.print(s + " ");
+        System.out.println();
+    }
+}
